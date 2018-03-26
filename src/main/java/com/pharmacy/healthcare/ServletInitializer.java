77efd8b52d3951e956dc199fe61c2a31ff4e8c81
Gies,Stefan S.T.W <s.gies@student.fontys.nl>
2018-03-26 13:43:59 +0200

    Retry

--- src/main/java/com/pharmacy/healthcare/ServletInitializer.java
+++ src/main/java/com/pharmacy/healthcare/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.pharmacy.healthcare;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
