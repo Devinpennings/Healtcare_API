5e2604d93f7fe64bf7f4aad640574d21b7299788
Gies,Stefan S.T.W <s.gies@student.fontys.nl>
2018-04-04 09:12:30 +0200

    Initial Update

--- src/main/java/com/pharmacy/healthcare/oauth/WebSecurityConfig.java
+++ src/main/java/com/pharmacy/healthcare/oauth/WebSecurityConfig.java
@@ -31,10 +31,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/login")
-				.and().ignoring().antMatchers("/patients/activate/**")
-		.and().ignoring().antMatchers("/patients/validate/**")
-		.and().ignoring().antMatchers("/_ah/**");
+		web.ignoring().antMatchers("/patients/activate/**")
+		.and().ignoring().antMatchers("/patients/validate/**");
 	}
 
 	@Override
